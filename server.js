5cb9cbaf22de7dcc85c5ccc539675a68df5c339c
Ahmed Qaddoura <hem-plo@hotmail.com>
2023-06-15 23:41:03 +0400

    limit requests

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const fs = require('fs');
 const app = express();
 const port = 1996;
 
+app.use(express.json({ limit: 1024 * 1024 }));
+
 // Set up Multer for file upload
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
